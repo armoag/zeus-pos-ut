e0b540a0409aa0dc8aee4d3efb781a453586ffab
Arturo Morales <amorales@Peregrine.int>
2019-02-08 17:30:46 -0800

    initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ZeusUT")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ZeusUT")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0332dc73-4998-4897-9afa-5ee02cf1d918")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace ZeusUT
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int numeroEsperado = 5;
+            var inventario = Athena.InventoryBase.GetInstance(@"C:\Projects\seiya-pos\Data\Inventario.csv");
+            var numero = inventario.GetLastItemNumber();
+            if (numero != numeroEsperado)
+                throw new Exception("error en numero de inv");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+
+        }
+    }
+}
